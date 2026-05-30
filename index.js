e7d4a79b25daaa9345c99385b7cd3333b2d9f591
Tamara12119 <marientamaraseguracarvajal@gmail.com>
2026-05-30 14:27:18 +0000

    + multiples idiomas

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
-import express from 'express'
+//import express from 'express'
+const express = require('express')
 
 const app = express()
+
 
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
+
+app.get('/es', (req, res) => {
+  res.send('Hola Mundo')
+})
+
+app.get('/it', (req, res) => {
+  res.send('Ciao mondo')
+})
+
+app.get('/jp', (req, res) => {
+  res.send('こんにちは世界')
+})
+
+app.get('/cn', (req, res) => {
+  res.send('你好，世界')
+})
+
+app.get('/ru', (req, res) => {
+  res.send('Привет, мир')
+})
+
+app.get('/ar', (req, res) => {
+  res.send('أهلاً بالعالم')
+})
+
+
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000')
